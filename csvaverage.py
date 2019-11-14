74ede15db0e61664af90ebd478be111f9e606b18
Guglielmo Pellegrino <zcemgsp@ucl.ac.uk>
2019-11-14 12:40:46 +0000

    first clean working version of mean calculator from cvs

--- csvaverage.py
+++ csvaverage.py
@@ -1,18 +1,13 @@
 import numpy as np
 
-import numpy as np
-
 path = 'data.csv'
 all= np.genfromtxt(path, delimiter=",")
 
-all.mean(axis=0)
+results=all.mean(axis=0)
 
-average=[]
 
-for i in range(all.shape[1]):
-    average.append((all[0,i]+all[1,i])/2)
     
- 
+print(results)
 file = open("results.txt","w+")
 file.write("average")
 file.close
