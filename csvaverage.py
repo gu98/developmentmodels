593f9e0e28e2c47e95ccacec11b389821d8d898b
Sandro Moszczynski <sandro.moszczynski.19@ucl.ac.uk>
2019-11-14 12:32:05 +0000

    Answers UCL-RITS/rsd-classwork-2019#59

--- csvaverage.py
+++ csvaverage.py
@@ -1,2 +1,18 @@
-import numpy 
-import numpy 
+import numpy as np
+
+import numpy as np
+
+path = 'data.csv'
+all= np.genfromtxt(path, delimiter=",")
+
+all.mean(axis=0)
+
+average=[]
+
+for i in range(all.shape[1]):
+    average.append((all[0,i]+all[1,i])/2)
+    
+ 
+file = open("results.txt","w+")
+file.write("average")
+file.close
